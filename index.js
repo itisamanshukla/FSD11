362656631163bef23d23e0e36c1346461a969f14
Aman Shukla <amanshukla03102005@gmail.com>
2026-02-05 16:26:35 +0530

    second commit

--- index.js
+++ index.js
@@ -1,1 +1,93 @@
-console.log("hello universe")
+// console.log("hello universe");
+// console.warn("This is a warnning message");
+// let num =5;
+// let bool=true;
+// let float=1.2;
+// let str="Aman";
+// let nul=null;
+// let und;
+// var e=20;
+// const f=30;
+// console.log(typeof(num));
+// console.log(typeof(bool));
+// console.log(typeof(float));
+// console.log(typeof(str));
+// console.log(typeof(e));
+// console.log(typeof(f));
+// console.log(typeof(nul));
+// console.log(typeof(und));
+// b=e+f;
+// console.log(b);
+// let x=10;
+// let y=2;
+// console.log(x-y);
+// console.log(x+y);
+// console.log(x/y);
+// console.log(x*y);
+// console.log(x%y);
+// let k=50;
+// var i1=20;
+// var i1=30;
+
+// console.log(k);
+// console.log(i1);
+
+// function add(p,q){
+//     return p+q;
+// }
+// console.log(add(2,3));
+
+//arrow function
+// const multiply=(p,q)=>p*q;
+// console.log("arrow function",multiply(5,5));
+
+// let arr=[10,20,30,40,50];
+// console.log(arr[0]);
+// arr.push(20);
+// arr.pop();
+// console.log(arr);
+
+//object
+// let student={
+//     name:"vikas",
+//     age:23,
+//     course:"btech"
+// };
+
+// console.log(student.name);
+
+// let today=new Date();
+// console.log(today);
+
+// console.time("test");
+// for(let i=0;i>100000;i++){
+
+// }
+// console.timeEnd("test");
+
+// console.group("User");
+// console.log("Name:Aman");
+// console.log("Age:24");
+// console.groupEnd();
+// console.log("hello");
+
+
+// console.count("a");
+// console.count("a");
+// console.count("b");
+// console.count("a");
+// console.count("c");
+// console.count("a");
+// console.count("a");
+
+
+//  let student1={
+//     name:"vikas",
+//     age:23,
+//     course:"btech"
+// };
+// console.table(student1);
+
+//leetcode github codechef linekdin
+
+
